5850c3d4a3af3553a2ba5f80acc1fd45d14e44e6
dandi ans <dandianastasa12@gmail.com>
2024-07-31 16:10:25 +0000

    feat: handle argument if it's a number, get atom info by its atomic_number

--- element.sh
+++ element.sh
@@ -6,4 +6,10 @@ if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
 else
   # TODO: get the info of an element from the given argument
+  # 1. check whether the argument is number or not
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    # get the info by its atomic number
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1")
+    echo "$ELEMENT"
+  fi
 fi
