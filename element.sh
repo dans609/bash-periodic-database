3071f7ca649fd9dd362daf8605d940bfb90b0789
dandi ans <dandianastasa12@gmail.com>
2024-07-31 16:47:18 +0000

    refactor: remove boilerplate code and separated the logic to handle empty info

--- element.sh
+++ element.sh
@@ -10,14 +10,12 @@ else
   if [[ $1 =~ ^[0-9]+$ ]]; then
     # get the info by its atomic number
     ELEMENT_INFO=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1")
-    if [[ -z $ELEMENT_INFO ]]; then
-      echo "I could not find that element in the database."
-    fi
   else
     # 2. get the info by its symbol OR its name
     ELEMENT_INFO=$($PSQL "SELECT * FROM elements WHERE symbol='$1' OR name='$1'")
-    if [[ -z $ELEMENT_INFO ]]; then
-      echo "I could not find that element in the database."
-    fi
+  fi
+
+  if [[ -z $ELEMENT_INFO ]]; then
+    echo "I could not find that element in the database."
   fi
 fi
