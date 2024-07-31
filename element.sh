a9b0d735c107f5d108bea409318d2e6a0b316039
dandi ans <dandianastasa12@gmail.com>
2024-07-31 17:24:09 +0000

    feat: get the detailed info by joinning all tables and print the result that associated with the info

--- element.sh
+++ element.sh
@@ -19,5 +19,11 @@ else
     echo "I could not find that element in the database."
   else
     #TODO: the record is found, then get detailed element information
+    echo $ELEMENT_INFO | while IFS="|" read ATOM SYMBOL NAME; do
+      DETAILED_INFO=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ATOM AND symbol='$SYMBOL' AND name='$NAME'")
+      echo $DETAILED_INFO | while IFS="|" read MASS MELT_PT BOIL_PT TYPE; do
+        echo "The element with atomic number $ATOM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT_PT celsius and a boiling point of $BOIL_PT celsius."
+      done
+    done
   fi
 fi
