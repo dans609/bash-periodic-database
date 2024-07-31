440d27fcafaa78268318377cd57e5c6855a9c300
dandi ans <dandianastasa12@gmail.com>
2024-07-31 16:12:32 +0000

    fix: dbname options value

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
