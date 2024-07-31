fea8244b2d501cf0670a9b09c2386e93ae37ce36
dandi ans <dandianastasa12@gmail.com>
2024-07-31 15:56:45 +0000

    feat: add an else statement to handle logic when the argument is given

--- element.sh
+++ element.sh
@@ -4,4 +4,6 @@ PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
+else
+  # TODO: get the info of an element from the given argument
 fi
